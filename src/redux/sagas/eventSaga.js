88ab4df229efdcbf3c5e7f3dbc50ab1362e2eb9f
diabloevagto <diabloevagto@gmail.com>
2018-10-08 02:27:01 +0800

    feat: 多個管理員 uuid

--- src/redux/sagas/eventSaga.js
+++ src/redux/sagas/eventSaga.js
@@ -24,7 +24,7 @@ function* getUUID() {
   yield put(
     actions.event.getUUID.SUCCESS({
       uuid,
-      isAdmin: uuid === adminUUID,
+      isAdmin: adminUUID.indexOf(uuid) != -1,
     }),
   );
 }
